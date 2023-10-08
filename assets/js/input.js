4830909db59b26a9be3cbfb2c9b97cfee7c1ca5f
Sselenso <122260649+Sselenso@users.noreply.github.com>
2023-10-08 18:04:07 +0300

    8.10_18:03

---
+++ assets/js/input.js
@@ -0,0 +1,78 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const messageInput = document.getElementById('message-text');
+  const sendButton = document.getElementById('send-button');
+  const chatWindow = document.querySelector('.chat-window');
+
+  messageInput.addEventListener('input', handleInput);
+  sendButton.addEventListener('click', handleSend);
+
+  function handleInput() {
+    if (messageInput.value.trim() !== '') {
+      sendButton.disabled = false;
+      sendButton.classList.add('active');
+    } else {
+      sendButton.disabled = true;
+      sendButton.classList.remove('active');
+    }
+  }
+
+  function handleSend() {
+    if (!sendButton.disabled) {
+      const messageText = messageInput.value;
+
+      if (messageText.trim() !== '') {
+        const specialistMessage = createSpecialistMessage(messageText);
+        chatWindow.appendChild(specialistMessage);
+
+        messageInput.value = '';
+        sendButton.disabled = true;
+        sendButton.classList.remove('active');
+
+        scrollToBottom();
+      }
+    }
+  }
+
+  messageInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      handleSend();
+    }
+  });
+
+  // Функция для прокручивания к самому нижнему сообщению
+  function scrollToBottom() {
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+  }
+});
+
+function createSpecialistMessage(messageText) {
+  const specialistMessage = document.createElement('div');
+  specialistMessage.classList.add('message__spec');
+
+  const messageContainer = document.createElement('div');
+  messageContainer.classList.add('specialist-message');
+
+  const messageInfo = document.createElement('div');
+  messageInfo.classList.add('message-info');
+
+  const username = document.createElement('span');
+  username.classList.add('username');
+  username.textContent = 'Специалист';
+
+  const timestamp = document.createElement('span');
+  timestamp.classList.add('timestamp');
+  const currentDate = new Date();
+  timestamp.textContent = currentDate.getHours() + ':' + currentDate.getMinutes() + ' ' + (currentDate.getHours() >= 12 ? 'PM' : 'AM');
+
+  const messageTextSpec = document.createElement('div');
+  messageTextSpec.classList.add('message-text-spec');
+  messageTextSpec.textContent = messageText;
+
+  messageInfo.appendChild(username);
+  messageInfo.appendChild(timestamp);
+  messageContainer.appendChild(messageInfo);
+  messageContainer.appendChild(messageTextSpec);
+  specialistMessage.appendChild(messageContainer);
+
+  return specialistMessage;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,128 +1,110 @@
-// script.js
-
-// Получаем ссылки на элементы страницы
-const specialistButton = document.querySelector('.specialist-button');
-const chatList = document.querySelector('.chat-list');
-const chatMessages = document.querySelector('.chat-messages');
-const previousChatButton = document.querySelector('.previous-chat-button');
-const messageInput = document.querySelector('.message-input input');
-const sendButton = document.querySelector('.message-input button');
-const editButton = document.querySelector('.edit-button');
-const releaseChatButton = document.querySelector('.release-chat-button');
-const closeChatButton = document.querySelector('.close-chat-button');
-const supportButton = document.querySelector('.support-button');
-const connectionButton = document.querySelector('.connection-button');
-
-// Обработчик нажатия на кнопку специалиста
-specialistButton.addEventListener('click', () => {
-    const activeChat = document.querySelector('.chat.active');
-    if (activeChat) {
-        const specialistName = specialistButton.textContent;
-        const chatName = activeChat.querySelector('.chat-name').textContent;
-        const confirmTransfer = confirm(`Вы уверены, что хотите передать чат "${chatName}" специалисту ${specialistName}?`);
-        if (confirmTransfer) {
-            // Код для передачи активного чата другому специалисту
-            activeChat.remove();
-            console.log(`Передача чата "${chatName}" специалисту ${specialistName}`);
-        }
-    }
+// При нажатии на кнопку "Мои чаты"
+document.querySelector('.my__chat').addEventListener('click', function() {
+  // Загрузка моих чатов
+  loadMyChats();
+  // Изменение активного класса
+  document.querySelector('.my__chat').classList.add('active');
+  document.querySelector('.all__chat').classList.remove('active');
 });
 
-// Обработчик нажатия на чат в списке
-chatList.addEventListener('click', (event) => {
-    const chat = event.target.closest('.chat');
-    if (chat) {
-        // Код для отображения сообщений выбранного чата
-        console.log('Отображение сообщений выбранного чата');
-        const activeChat = document.querySelector('.chat.active');
-        if (activeChat) {
-            activeChat.classList.remove('active');
-        }
-        chat.classList.add('active');
-    }
+// При нажатии на кнопку "Общие чаты"
+document.querySelector('.all__chat').addEventListener('click', function() {
+  // Загрузка всех чатов
+  loadAllChats();
+  // Изменение активного класса
+  document.querySelector('.all__chat').classList.add('active');
+  document.querySelector('.my__chat').classList.remove('active');
 });
 
-// Обработчик нажатия на кнопку "Прошлый чат"
-previousChatButton.addEventListener('click', () => {
-    const activeChat = document.querySelector('.chat.active');
-    if (activeChat) {
-        const previousChat = activeChat.previousElementSibling;
-        if (previousChat && previousChat.classList.contains('chat')) {
-            // Код для переключения на предыдущий чат
-            activeChat.classList.remove('active');
-            previousChat.classList.add('active');
-            console.log('Переключение на предыдущий чат');
-        }
-    }
+// Загрузка моих чатов
+function loadMyChats() {
+  // Код для загрузки моих чатов
+}
+
+// Загрузка всех чатов
+function loadAllChats() {
+  // Код для загрузки всех чатов
+}
+
+// При нажатии на чат
+document.querySelectorAll('.chat').forEach(function(chat) {
+  chat.addEventListener('click', function() {
+    // Перенос чата в раздел "Мои чаты"
+    moveChatToMyChats(chat);
+  });
 });
 
-// Обработчик нажатия на кнопку "Отправить"
-sendButton.addEventListener('click', () => {
-    const messageText = messageInput.value;
-    if (messageText.trim() !== '') {
-        // Код для отправки сообщения
-        console.log(`Отправка сообщения: ${messageText}`);
-        const activeChat = document.querySelector('.chat.active');
-        if (activeChat) {
-            const messageHistory = activeChat.querySelector('.message-history');
-            const messageElement = document.createElement('div');
-            messageElement.classList.add('message');
-            messageElement.textContent = messageText;
-            messageHistory.appendChild(messageElement);
-        }
-        messageInput.value = '';
-    }
+// Перенос чата в раздел "Мои чаты"
+function moveChatToMyChats(chat) {
+  // Код для переноса чата в раздел "Мои чаты"
+}
+
+// При нажатии на кнопку "Текущий чат"
+document.querySelector('.realtime-chat-button').addEventListener('click', function() {
+  // Загрузка активного чата
+  loadActiveChat();
 });
 
-// Обработчик нажатия на кнопку "Редактировать"
-editButton.addEventListener('click', () => {
-    // Код для редактирования информации о клиенте
-    console.log('Редактирование информации о клиенте');
+// При нажатии на кнопку "Прошлый чат"
+document.querySelector('.previous-chat-button').addEventListener('click', function() {
+  // Загрузка прошлого чата
+  loadPreviousChat();
 });
 
-// Обработчик нажатия на кнопку "Отказаться от чата"
-releaseChatButton.addEventListener('click', () => {
-    const activeChat = document.querySelector('.chat.active');
-    if (activeChat) {
-        const confirmRelease = confirm('Вы уверены, что хотите отказаться от этого чата?');
-        if (confirmRelease) {
-            // Код для отказа от чата
-            activeChat.classList.remove('active');
-            chatList.appendChild(activeChat);
-            console.log('Отказ от чата');
-        }
-    }
+// Загрузка активного чата
+function loadActiveChat() {
+  // Код для загрузки активного чата
+}
+
+// Загрузка прошлого чата
+function loadPreviousChat() {
+  // Код для загрузки прошлого чата
+}
+
+// При вводе текста в поле поиска по чату
+document.querySelector('.search__history').addEventListener('input', function() {
+  // Фильтрация сообщений по введенному тексту
+  filterChatMessages(this.value);
 });
 
-// Обработчик нажатия на кнопку "Закрыть чат"
-closeChatButton.addEventListener('click', () => {
-    const activeChat = document.querySelector('.chat.active');
-    if (activeChat) {
-        const confirmClose = confirm('Вы уверены, что хотите закрыть этот чат?');
-        if (confirmClose) {
-            // Код для закрытия чата
-            activeChat.remove();
-            console.log('Закрытие чата');
-        }
-    }
+// Фильтрация сообщений по введенному тексту
+function filterChatMessages(text) {
+  // Код для фильтрации сообщений по введенному тексту
+}
+
+// При вводе текста в поле ввода сообщения
+document.querySelector('#message-text').addEventListener('input', function() {
+  // Активация/деактивация кнопки отправки сообщения
+  toggleSendButton();
 });
 
-// Обработчик нажатия на кнопку "Поддержка"
-supportButton.addEventListener('click', () => {
-    const activeChat = document.querySelector('.chat.active');
-    if (activeChat) {
-        const chatTypeIndicator = activeChat.querySelector('.chat-type-indicator');
-        chatTypeIndicator.style.backgroundColor = 'blue';
-        console.log('Изменение типа чата на "Поддержка"');
-    }
+// Активация/деактивация кнопки отправки сообщения
+function toggleSendButton() {
+  var messageText = document.querySelector('#message-text').value;
+  var sendButton = document.querySelector('#send-button');
+  
+  if (messageText.trim() !== '') {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
+}
+
+// При нажатии на кнопку отправки сообщения
+document.querySelector('#send-button').addEventListener('click', function() {
+  // Отправка сообщения
+  sendMessage();
 });
 
-// Обработчик нажатия на кнопку "Подключение"
-connectionButton.addEventListener('click', () => {
-    const activeChat = document.querySelector('.chat.active');
-    if (activeChat) {
-        const chatTypeIndicator = activeChat.querySelector('.chat-type-indicator');
-        chatTypeIndicator.style.backgroundColor = 'green';
-        console.log('Изменение типа чата на "Подключение"');
-    }
-});
+// Отправка сообщения
+function sendMessage() {
+  var messageText = document.querySelector('#message-text').value;
+  
+  // Код для отправки сообщения
+  
+  // Очистка поля ввода сообщения
+  document.querySelector('#message-text').value = '';
+  // Деактивация кнопки отправки сообщения
+  toggleSendButton();
+}
+
